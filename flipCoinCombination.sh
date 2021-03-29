4c15ec60a225bb981d2cee4917cae888b8b88665
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-29 22:28:27 +0530

    Addition UC4

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,37 +2,59 @@
 
 declare -A DoubleDice
 declare -A PercentDouble
-hh=0
-ht=0
-th=0
-tt=0
-
+hhh=0
+hth=0
+thh=0
+hht=0
+ttt=0
+tth=0
+tht=0
+htt=0
 for ((count=0; count<20; count++))
 do
-	result=$((RANDOM % 4))
+	result=$((RANDOM % 8))
 	if [ $result -eq 0 ]
 	then
-		$((hh++))
+		hhh=$(($hhh+1))
 	elif [ $result -eq 1 ]
 	then
-		$((ht++))
+		hth=$(($hth+1))
 	elif [ $result -eq 2 ]
 	then
-		$((th++))
+		thh=$(($thh+1))
 	elif [ $result -eq 3 ]
 	then
-		$((tt++))
+		hht=$(($hht+1))
+	elif [ $result -eq 4 ]
+   then
+      ttt=$(($ttt+1))
+	 elif [ $result -eq 5 ]
+   then
+      tth=$(($tth+1))
+	 elif [ $result -eq 6 ]
+   then
+      tht=$(($tht+1))
+	 elif [ $result -eq 7 ]
+   then
+      htt=$(($htt+1))
 	fi
 done
 
-DoubleDict[HH]=$hh
-DoubleDict[TT]=$tt
-DoubleDict[HT]=$ht
-DoubleDict[TH]=$th
+DoubleDict[HHH]=$hhh
+DoubleDict[TTT]=$ttt
+DoubleDict[HTH]=$hth
+DoubleDict[THH]=$thh
+DoubleDict[HHT]=$hht
+DoubleDict[TTH]=$tth
+DoubleDict[THT]=$tht
+DoubleDict[HTT]=$htt
 
-for i in ${!DoubleDict[@]}
+
+
+for i in ${!DoubleDict[@]};
 do
 	PercentDouble[$i]=$((${DoubleDict[$i]} * 100 / 20))
+	echo $i" percentage: "${DoubleDict[$i]}
 done
 echo ${PercentDouble[@]}
 
